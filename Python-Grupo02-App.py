3d8620dd85b3c7c65d75a589c29681f5d6c8a23f
miguelvg27 <miguelvg27@gmail.com>
2023-11-21 23:32:59 -0500

    plt

--- Python-Grupo02-App.py
+++ Python-Grupo02-App.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 @st.cache_data
 def cargar_df():
@@ -8,4 +10,20 @@ def cargar_df():
     return df
 	
 df = cargar_df()
-st.write(df)
+
+# Sidebar para filtros
+st.sidebar.header('Filtros')
+estacion = st.sidebar.selectbox('Elige una Estación de Monitoreo:', df['Estación de monitoreo'].unique())
+contaminante = st.sidebar.selectbox('Elige un Contaminante:', ['PM2.5 (ug/m3)', 'CO (ug/m3)', 'NO2 (ug/m3)', 'O3 (ug/m3)'])
+
+# Filtrar los datos
+df_filtrado = df[df['Estación de monitoreo'] == estacion]
+
+# Crear el gráfico
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df_filtrado, x='Fecha', y=contaminante)
+plt.xticks(rotation=45)
+plt.title(f'Concentración de {contaminante} en {estacion} a lo Largo del Tiempo')
+st.pyplot(plt)
+
+st.write(df_filtrado)
