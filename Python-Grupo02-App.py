2516d7c0e09def50e5c210cefd0e0c754d84665e
miguelvg27 <miguelvg27@gmail.com>
2023-11-21 23:00:10 -0500

    df_nan

--- Python-Grupo02-App.py
+++ Python-Grupo02-App.py
@@ -4,7 +4,7 @@ import numpy as np
 
 @st.cache_data
 def cargar_df():
-    df = pd.read_csv('./data/Consolidado-Monitoreo-Miraflores-QAIRA-1.csv')
+    df = pd.read_csv('./data/Consolidado-Monitoreo-Miraflores-QAIRA-1.csv', na_values='')
     return df
 	
 df = cargar_df()
