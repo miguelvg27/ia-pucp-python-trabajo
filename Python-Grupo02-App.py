3cb20b2c8ddff2e3d01ccef24603ca6f8abbe6c2
miguelvg27 <miguelvg27@gmail.com>
2023-11-22 00:15:24 -0500

    miguel

--- Python-Grupo02-App.py
+++ Python-Grupo02-App.py
@@ -10,15 +10,38 @@ def cargar_df():
     df['Fecha'] = pd.to_datetime(df['Fecha'], format='%d/%m/%Y %H:%M')
     return df
 
+st.title('Calidad de aire en Miraflores')
+st.write('Realiza tus consultas y descarga la información')
+
 df = cargar_df()
+# Determina el rango mínimo y máximo de fechas
+fecha_min = df['Fecha'].min().date()
+fecha_max = df['Fecha'].max().date()
 
 # Sidebar para filtros
 st.sidebar.header('Filtros')
-estacion = st.sidebar.selectbox('Elige una Estación de Monitoreo:', df['Estación de monitoreo'].unique())
-contaminante = st.sidebar.selectbox('Elige un Contaminante:', ['PM2.5 (ug/m3)', 'CO (ug/m3)', 'NO2 (ug/m3)', 'O3 (ug/m3)'])
+estacion = st.sidebar.selectbox(
+    'Elige una Estación de Monitoreo:', 
+	df['Estación de monitoreo'].unique(),
+	index=1
+)
+contaminante = st.sidebar.selectbox(
+	'Elige un Contaminante:', 
+	['PM2.5 (ug/m3)', 'CO (ug/m3)', 'NO2 (ug/m3)', 'O3 (ug/m3)'],
+	index=2
+)
+fecha_seleccionada = st.sidebar.slider(
+    'Selecciona un rango de fechas:',
+    min_value=fecha_min,
+    max_value=fecha_max,
+    value=(pd.to_datetime('2021-06-01').date(), pd.to_datetime('2021-06-30').date())
+)
+
 
 # Filtrar los datos
-df_filtrado = df[df['Estación de monitoreo'] == estacion]
+df_filtrado = df[(df['Estación de monitoreo'] == estacion) & 
+                 (df['Fecha'].dt.date >= fecha_seleccionada[0]) & 
+                 (df['Fecha'].dt.date <= fecha_seleccionada[1])]
 
 # Crear el gráfico
 plt.figure(figsize=(10, 6))
