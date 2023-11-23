ba34fa471b2f95eff6d905c20a9e73dafe24fecb
ray36512 <40697774+ray36512@users.noreply.github.com>
2023-11-23 20:23:05 +0100

    Update Python-Grupo02-App.py

--- Python-Grupo02-App.py
+++ Python-Grupo02-App.py
@@ -9,7 +9,7 @@ from datetime import datetime
 #subprocess.run(["pip", "install", "pandas", "folium", "streamlit_folium", "geopandas", "ipyleaflet", "Map", "GeoJSON", "Marker", "MarkerCluster"])
 #import folium
 #from streamlit_folium import folium_static
-import geopandas as gpd
+#import geopandas as gpd
 #from ipyleaflet import Map, GeoJSON, Marker, MarkerCluster
 
 @st.cache_data
