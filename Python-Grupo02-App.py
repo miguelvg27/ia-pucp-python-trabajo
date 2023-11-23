d03cb797abb61666820b1daa255b4609883a2a76
ray36512 <40697774+ray36512@users.noreply.github.com>
2023-11-23 20:44:00 +0100

    Update Python-Grupo02-App.py

--- Python-Grupo02-App.py
+++ Python-Grupo02-App.py
@@ -115,7 +115,7 @@ plt.xticks(rotation=0)
 st.pyplot(plt)
 
 df = pd.DataFrame(
-    np.random.randn(1000, 2) / [0, 0] + [-12.0727, -77.0827],
+    np.random.randn(1000, 2) / [1, 1] + [-12.0727, -77.0827],
     columns=['lat', 'lon'])
 
 st.map(df)
