7fb9a7df20758b335a24025090250449ee26a312
ray36512 <40697774+ray36512@users.noreply.github.com>
2023-11-23 16:21:46 +0100

    Update Python-Grupo02-App.py

--- Python-Grupo02-App.py
+++ Python-Grupo02-App.py
@@ -107,3 +107,10 @@ plt.title(f"Promedio de Humedad de emisiones de {contaminante} en {estacion}")
 plt.xticks(rotation=0)
 st.pyplot(plt)
 
+# Gráfico 5
+plt.figure(figsize=(10, 4))
+sns.heatmap(df_filtrado[['Temperatura', 'Humedad']].corr(), annot=True)
+plt.title(f"Promedio de Humedad de emisiones de {contaminante} en {estacion}")
+plt.xticks(rotation=0)
+st.pyplot(plt)
+
