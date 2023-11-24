77802b39fbba2502ce433410a5ff5dabd1d6d9b1
ray36512 <40697774+ray36512@users.noreply.github.com>
2023-11-24 21:21:03 +0100

    Update Python-Grupo02-App.py

--- Python-Grupo02-App.py
+++ Python-Grupo02-App.py
@@ -240,6 +240,7 @@ df4['contaminante'] = np.random.randn(num_repeticiones)
 st.map(df_miraflores,
     latitude='Latitud',
     longitude='Longitud',
+    zoom = 14,
     use_container_width=True)
 
 st.map(df4,
