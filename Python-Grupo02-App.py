d9ef8cb05a86822611d63c0273e80c61acd779d4
ray36512 <40697774+ray36512@users.noreply.github.com>
2023-11-25 00:04:26 +0100

    Update Python-Grupo02-App.py

--- Python-Grupo02-App.py
+++ Python-Grupo02-App.py
@@ -137,13 +137,13 @@ st.pyplot(plt)
 #Dataset
 st.write(df_miraflores) 
 
-fecha_inicio_str = fecha_seleccionada[0].strftime("%d/%m/%Y")
-fecha_fin_str = fecha_seleccionada[1].strftime("%d/%m/%Y")
+fecha_inicio = fecha_seleccionada[0].strftime("%d/%m/%Y")
+fecha_fin = fecha_seleccionada[1].strftime("%d/%m/%Y")
 
 
 # Mapa automatico que cambia con el Slide de contaminantes y Estacion
 #Grafico 7
-st.subheader(f'Mapa interactivo de emisiones de {contaminante} en {estacion} a las {hora_seleccionada} horas desde {fecha_inicio_str} hasta {fecha_fin_str} ')
+st.subheader(f'Mapa interactivo de emisiones de {contaminante} en {estacion} a las {hora_seleccionada} horas desde {fecha_inicio} hasta {fecha_fin}')
 st.map(df_miraflores_hora,
     latitude='Latitud',
     longitude='Longitud',
